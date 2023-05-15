7caa6bb52a9eda5e1d959690e18af9ee8d05c869
Sham Vijay <benzwrites@gmail.com>
2023-05-15 23:48:04 +0530

    Setting up MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
+import "dotenv/config"
+import mongoose from "mongoose";
+
 import express  from "express";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 app.get("/",(req,res) => {
     res.send("HELLO5 WORLD!!");
 });
-
-app.listen(port,() => {
-    console.log("Server running on port : "+port);
-})
+mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+    .then(()=>{
+        app.listen(port,() => {
+            console.log("Server running on port : "+port);
+        })
+    })
+    .catch(console.error)
