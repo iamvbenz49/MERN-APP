d348b890ad5d1c3549547ae41b91df00af19a15d
Sham Vijay <benzwrites@gmail.com>
2023-05-17 22:48:48 +0530

    created node.ts and app.ts

---
+++ src/models/node.ts
@@ -0,0 +1,10 @@
+import { InferSchemaType, Schema } from "mongoose"
+
+const nodeSchema = new Schema({
+    title:{type:String,required:true},
+    text:{type:String},
+},{timestamps:true});
+
+type Note = InferSchemaType<typeof nodeSchema>;
+
+export default model<Note>("Note",nodeSchema);
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
-import "dotenv/config"
+import app from "./app";
 import mongoose from "mongoose";
+import env from "./utils/validateEnv";
 
-import express  from "express";
 
-const app = express();
 const port = process.env.PORT;
 
-app.get("/",(req,res) => {
-    res.send("HELLO5 WORLD!!");
-});
-mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+mongoose.connect(env.MONGO_CONNECTION_STRING!)
     .then(()=>{
         app.listen(port,() => {
             console.log("Server running on port : "+port);
